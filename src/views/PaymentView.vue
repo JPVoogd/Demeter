4f6597083a3d1bd59eae91d6ee19be0a78eb07ba
Jeroen Voogd <50957295+JPVoogd@users.noreply.github.com>
2024-04-13 17:23:30 +0200

    small fix

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -7,12 +7,12 @@
     <p>The price is: €{{ useProductStore.price }},-</p>
     <label>
       <input
-          type="text"
-          class="linear-payment payment-form"
-          v-model="cardNumber"
-          placeholder="Enter your card details"
-      /></label>
-    <br/>
+        type="text"
+        class="linear-payment payment-form"
+        v-model="cardNumber"
+        placeholder="Enter your card details"
+    /></label>
+    <br />
     <button @click="paymentCard(cardNumber)">Pay now!</button>
   </div>
 
@@ -20,32 +20,42 @@
     <p>You are buying: {{ useProductStore.name }}</p>
     <p>The price is: €{{ useProductStore.price }}</p>
     <label>
-      <input type="text" class="linear-payment payment-form" v-model="inputPayment" placeholder="Enter cash amount"
-      /></label>
-    <br/>
+      <input
+        type="text"
+        class="linear-payment payment-form"
+        v-model="inputPayment"
+        placeholder="Enter cash amount"
+    /></label>
+    <br />
     <button @click="paymentCash(inputPayment)">Pay now!</button>
   </div>
 </template>
 
 <script setup>
 import router from "@/router";
-import {useProductStore} from "@/stores/ProductStore";
+import { useProductStore } from "@/stores/ProductStore";
 
 async function paymentCard(cardNumber) {
   alert(
-      "The following amount: €" +
+    "The following amount: €" +
       useProductStore.price +
       " will be taken from account: " +
       cardNumber +
       " ."
   );
+  useProductStore.card = false;
   router.push("container");
 }
 
 async function paymentCash(inputPayment) {
-  const change = inputPayment - useProductStore.price;
-  alert("Your change is: €" + parseFloat(change) + " .");
-  router.push("container");
+  if (inputPayment < useProductStore.price) {
+    alert("You don't have enough money to buy this product.");
+  } else {
+    const change = inputPayment - useProductStore.price;
+    alert("Your change is: €" + parseFloat(change).toFixed(2) + " .");
+    useProductStore.cash = false;
+    router.push("container");
+  }
 }
 </script>
 
@@ -65,7 +75,7 @@ async function paymentCash(inputPayment) {
 
 .linear-payment {
   background: linear-gradient(to right bottom, #ebe9e9, #bbceee) padding-box,
-  linear-gradient(90deg, #085889, #089c6d) border-box;
+    linear-gradient(90deg, #085889, #089c6d) border-box;
   border-radius: 10px;
   border: 1px solid transparent;
 }
